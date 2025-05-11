51a741ad2aa11ba05261b5665254e8f888d454ff
tumee <tmkee0525@gmail.com>
2025-05-11 23:21:39 +0900

    route change

--- app/Providers/Filament/AppPanelProvider.php
+++ app/Providers/Filament/AppPanelProvider.php
@@ -28,7 +28,7 @@ class AppPanelProvider extends PanelProvider
         return $panel
             ->default()
             ->id('app')
-            ->path('app')
+            ->path('')
             ->brandName('SENGUM')
             ->topNavigation()
             ->breadcrumbs(false)
